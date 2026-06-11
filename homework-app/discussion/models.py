5d16fe9ed6d973353e464e19822473588a81c637
mrinalinikirthi-coder <mrinalini.kirthi@example.com>
2026-06-11 12:25:52 +0530

    models created

--- homework-app/discussion/models.py
+++ homework-app/discussion/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
-# Create your models here.
+class Teachers(models.Model):
+    T_id=models.CharField(max_length=10,unique=True,primary_key=True)
+    T_name=models.CharField(max_length=75)
+    Subject=models.CharField(max_length=25)
+    Qualification=models.CharField(max_length=30)
+    Years_of_Exp=models.SmallIntegerField()
+    Gender=models.CharField(max_length=10)
+    Mail=models.EmailField(unique=True)
+    Log_Pass=models.CharField(max_length=300)
