792ba534e765aa06c169ae03ab4e104b05e02338
mrinalinikirthi-coder <mrinalini.kirthi@example.com>
2026-06-11 16:53:53 +0530

    More models created

--- homework-app/discussion/models.py
+++ homework-app/discussion/models.py
@@ -1,25 +1,25 @@
     from django.db import models
     class Teacher(models.Model):
         gen_choices=[('M','Male'),('F','Female'),('O',"Other")]
-        t_name=models.CharField(max_length=75)
+        name=models.CharField(max_length=75)
         qualification=models.CharField(max_length=30)
         years_of_exp=models.SmallIntegerField()
         gender=models.CharField(max_length=10,choices=gen_choices)
-        t_Mail=models.EmailField(unique=True)
+        mail=models.EmailField(unique=True)
         log_pass=models.CharField(max_length=300)
-        t_nationality=models.CharField(max_length=30)
+        nationality=models.CharField(max_length=30)
         sub_id=models.ForeignKey('Subject',on_delete=models.Cascade)
     class Student(models.Model):
         gen_choices=[('M','Male'),('F','Female'),('O',"Other")]
-        gr_choices=[('I','1st'),('II','2nd'),('III','3rd'),('IV','4th'),('V','5th'),('VI','6th')
+        gr_choices=[('I','1st'),('II','2nd'),('III','3rd'),('IV','4th'),('V','5th'),('VI','6th'),
         ('VII','7th'),('VIII','8th'),('IX','9th'),('X','10th'),('XI','11th'),('XII','12th')]
-        s_name=models.CharField(max_length=75)
+        name=models.CharField(max_length=75)
         grade=models.CharField(max_length=8,choices=gr_choices)
         school_name=models.CharField(max_length=50)
         login_pass=models.CharField(max_length=300)
         gender=models.CharField(max_length=10,choices=gen_choices)
-        s_Mail=models.EmailField(unique=True)
-        s_nationality=models.CharField(max_length=30)
+        mail=models.EmailField(unique=True)
+        nationality=models.CharField(max_length=30)
         sub_id=models.ForeignKey('Subject',on_delete=models.Cascade)
     class Subject(models.Model):
         sub_name=models.CharField(max_length=50)
