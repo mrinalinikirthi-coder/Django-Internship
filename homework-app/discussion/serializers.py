14e378fd21ca5071f2b9101f72c66e1f4ecbffc3
mrinalinikirthi-coder <mrinalini.kirthi@example.com>
2026-06-12 15:31:31 +0530

    Halfway

--- homework-app/discussion/serializers.py
+++ homework-app/discussion/serializers.py
@@ -3,4 +3,20 @@ from .models import Subject
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model=Subject
-        fields=['id','sub_name']
+        fields=['id','sub_name']    
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Student
+        fields='__all__'
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Teacher
+        fields='__all__'
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Post
+        fields='__all__'
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Reply
+        fields='__all__'
