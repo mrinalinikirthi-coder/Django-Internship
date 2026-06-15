b2c68e702d7967c1ab973d73b3aeab595c480538
mrinalinikirthi-coder <mrinalini.kirthi@example.com>
2026-06-15 13:52:00 +0530

    created views for all the models

--- homework-app/discussion/views.py
+++ homework-app/discussion/views.py
@@ -5,9 +5,28 @@ PostSerializer,ReplySerializer
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-class SubjectViewSet(viewsets.ModelViewSet):
-    queryset=Subject.object.all()
+class SubjectViewSet(ModelViewSet):
+    queryset=Subject.objects.all()
     serializer_class=SubjectSerializer
     authentication_classes=[]
     permission_classes=[]
-    
+class PostViewSet(ModelViewSet):
+    queryset=Post.objects.all()
+    serializer_class=PostSerializer
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
+class ReplyViewSet(ModelViewSet):
+    queryset=Reply.objects.all()
+    serializer_class=ReplySerializer
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
+class TeacherViewSet(ModelViewSet):
+    queryset=Teacher.objects.all()
+    serializer_class=TeacherSerializer
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
+class StudentViewSet(ModelViewSet):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
