47f013290027d697fa5c63e5cfae12cfe9f24325
mrinalinikirthi-coder <mrinalini.kirthi@example.com>
2026-06-15 12:26:28 +0530

    created views for subject class

--- homework-app/discussion/views.py
+++ homework-app/discussion/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from .models import Teacher,Student,Subject,Post,Reply
+from .serializers import SubjectSerializer,UserSerializer,StudentSerializer,TeacherSerializer,
+PostSerializer,ReplySerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+class SubjectViewSet(viewsets.ModelViewSet):
+    queryset=Subject.object.all()
+    serializer_class=SubjectSerializer
+    authentication_classes=[]
+    permission_classes=[]
+    
