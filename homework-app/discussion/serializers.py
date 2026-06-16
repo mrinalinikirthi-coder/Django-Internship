5c0c0764abb384c3b4356185c28eec6ddafecc78
mrinalinikirthi-coder <mrinalini.kirthi@example.com>
2026-06-16 16:32:13 +0530

    Creating login serializer and views

--- homework-app/discussion/serializers.py
+++ homework-app/discussion/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Subject,Student,Teacher,Post,Reply
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
@@ -35,3 +36,16 @@ class RegisterSerializer(serializers.Serializer):
         user=User.objects.create_user(username=validated_data['username'] , email = validated_data['email'],password = validated_data['password'])
         token=Token.objects.create(user=user)
         return {"user" : user , "token" : token}
+class LoginSerializer(serializers.Serializer):
+    username=serializers.CharField()
+    password=serializers.CharField(write_only=True)
+    def validate(self,attrs):
+        username=attrs['username']
+        password=attrs['password']
+        user=authenticate(username=username,password=password)
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        if not user.is_active:
+            raise serializers.ValidationError("Invalid credentials")
+        token,created=Token.objects.get_or_create(user=user)
+        return {"user" : user.username , "token" : token.key}
