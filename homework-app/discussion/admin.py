9f92d1946f64d4f81c3de61b1e3901d659a6f754
mrinalinikirthi-coder <mrinalini.kirthi@example.com>
2026-06-17 12:07:31 +0530

    testing the completed API

--- homework-app/discussion/admin.py
+++ homework-app/discussion/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Teacher,Student,Subject,Reply,Post
+admin.register(Student)
+admin.register(Teacher)
+admin.register(Subject)
+admin.register(Reply)
+admin.register(Post)
--- homework-app/discussion/views.py
+++ homework-app/discussion/views.py
@@ -60,7 +60,7 @@ class RegisterView(APIView):
         serializer=RegisterSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.validated_data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 class LoginView(APIView):
     authentication_classes=[]
