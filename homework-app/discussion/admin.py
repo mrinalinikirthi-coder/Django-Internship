217fccfe8837bc6566a72490a2124a4035d38c23
mrinalinikirthi-coder <mrinalini.kirthi@example.com>
2026-06-17 12:08:38 +0530

    testing the completed API

--- homework-app/discussion/admin.py
+++ homework-app/discussion/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Teacher,Student,Subject,Reply,Post
-admin.register(Student)
-admin.register(Teacher)
-admin.register(Subject)
-admin.register(Reply)
-admin.register(Post)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Subject)
+admin.site.register(Reply)
+admin.site.register(Post)
