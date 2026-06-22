4349ad10e077d1539469c213e89d2dd88e6dc753
mrinalinikirthi-coder <mrinalini.kirthi@example.com>
2026-06-22 12:26:02 +0530

    Adding .env to gitingnore

--- homework-app/discussion/urls.py
+++ homework-app/discussion/urls.py
@@ -1,14 +1,25 @@
-from django.urls import path,include
+from django.urls import path
+
 from rest_framework.routers import DefaultRouter
-from .views import SubjectViewSet,PostViewSet,ReplyViewSet,TeacherViewSet,StudentViewSet,RegisterView,LoginView
-router=DefaultRouter()
-router.register('posts',PostViewSet)
-router.register('replies',ReplyViewSet)
-router.register('students',StudentViewSet)
-router.register('teachers',TeacherViewSet)
-router.register('subjects',SubjectViewSet)
-urlpatterns=[
-    path('register/',RegisterView.as_view()),
-    path('login/',LoginView.as_view()),
+
+from .views import SubjectViewSet, PostViewSet, ReplyViewSet
+
+from .views import TeacherViewSet, StudentViewSet, RegisterView, LoginView
+
+router = DefaultRouter()
+
+router.register('posts', PostViewSet)
+
+router.register('replies', ReplyViewSet)
+
+router.register('students', StudentViewSet)
+
+router.register('teachers', TeacherViewSet)
+
+router.register('subjects', SubjectViewSet)
+
+urlpatterns = [
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
 ]
-urlpatterns+=router.urls
+urlpatterns += router.urls
--- homework-app/homework/settings.py
+++ homework-app/homework/settings.py
@@ -11,19 +11,19 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=kqeoupwyl50&*)&@l#=%y&mbm3wkm#rt2dc0a2lp(v641mnw*'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG','False') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -87,16 +87,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'NumericPasswordValidator',
     },
 ]
 
