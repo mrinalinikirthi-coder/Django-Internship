672483782f040cd7c5506027ae93b6e15ec92555
mrinalinikirthi-coder <mrinalini.kirthi@example.com>
2026-06-25 12:27:20 +0530

    Finalising the PEP 8 changes for settings.py

--- doubt_discussion_app/discussion_project/settings.py
+++ doubt_discussion_app/discussion_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
 
 # Load environment variables
 load_dotenv()
@@ -189,7 +190,6 @@ REST_FRAMEWORK = {
     ),
 }
 
-from datetime import timedelta
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
